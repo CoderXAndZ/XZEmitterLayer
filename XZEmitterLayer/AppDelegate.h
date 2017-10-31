f09c764133cbab531a1854713719ff983bddc997
ios_domain <1935722630@qq.com>
2017-10-31 16:25:59 +0800

    Initial Commit

---
+++ XZEmitterLayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZEmitterLayer
+//
+//  Created by admin on 2017/10/31.
+//  Copyright © 2017年 XZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XZEmitterLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZEmitterLayer
+//
+//  Created by admin on 2017/10/31.
+//  Copyright © 2017年 XZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
